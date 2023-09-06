af62d6d3f7da2ea1ef324568df11d2baea221e6d
VirtualcomGames <virtualcom.games@gmail.com>
2023-09-06 14:27:08 +0200

    Impero Romano Full RP
    Caricamento  manifest + dati veicoli

---
+++ Impero Romano Full RP/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+game { 'gta5' }
+
+files {
+    'data/**/*.meta'
+} 
+
+data_file 'HANDLING_FILE' 'data/**/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/**/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/**/**/dlctext.meta'
+data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/**/carcontentunlocks.meta'
---
+++ Impero Romano Full RP/data/[moto]/BMW_G310R/vehicle_names.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+	--  gs310r
+	AddTextEntry('0x873008B7', 'BMW G 310R')
+end)
