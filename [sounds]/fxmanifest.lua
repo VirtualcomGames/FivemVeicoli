320a73b92f4de989e8092dafb620afdd3a3f817e
VirtualcomGames <virtualcom.games@gmail.com>
2023-09-08 22:04:15 +0200

    aggiunta suoni veicoli

---
+++ [sounds]/fxmanifest.lua
@@ -0,0 +1,64 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/**.dat151.rel',
+  'audioconfig/**.dat54.rel',
+  'sfx/**/**.awc',
+}
+
+-- Veicoli: A45s,Golf 8-R, RS3
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ea888_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea888_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea888'
+
+-- Veicoli: Lamborghini SVJ, R8 
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lambov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lambov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lambov10'
+
+-- Veicoli: RS4, RS5
+data_file 'AUDIO_GAMEDATA' 'audioconfig/v6audiea839_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/v6audiea839_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_v6audiea839'
+
+-- Veicoli: M2
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/s55b30_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s55b30_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s55b30_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s55b30'
+
+-- Veicoli: Giulia Qv
+data_file 'AUDIO_GAMEDATA' 'audioconfig/alfa690t_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/alfa690t_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_alfa690t'
+
+-- Veicoli: GTR R34
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/bnr34ffeng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bnr34ffeng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/bnr34ffeng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bnr34ffeng'
+
+-- Veicoli: Lamborghini Tecnica
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/talam52v10_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/talam52v10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/talam52v10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_talam52v10'
+
+-- Veicoli: bently Mansory
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg125mnsrybently_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg125mnsrybently_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg125mnsrybently_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg125mnsrybently"
+
+-- Veicoli: Mustang Shelby
+data_file 'AUDIO_GAMEDATA' 'audioconfig/fordvoodoo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/fordvoodoo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_fordvoodoo'
+
+-- Veicoli: Pack Willy
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/kc27willys4_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/kc27willys4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/kc27willys4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_kc27willys4'
